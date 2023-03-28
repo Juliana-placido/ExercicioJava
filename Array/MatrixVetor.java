66eab2c8281d16803ca0c355275693cd4f1b775f
Juliana-placido <juliana.lplacido@icloud.com>
2023-03-28 19:06:28 -0300

    matrizVetor

--- Array/MatrixVetor.java
+++ Array/MatrixVetor.java
@@ -1,10 +1,40 @@
 package Generation.Array;
+import java.util.Scanner;
 
-public class MatrixVetor {
+public class ExercicioVetor {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int[] numeros = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Digite o %dº número inteiro: ", i+1);
+            numeros[i] = leia.nextInt();
+        }
 
-	}
+        System.out.print("Elementos de índices ímpares do vetor: ");
+        for (int i = 1; i < 10; i += 2) {
+            System.out.print(numeros[i] + " ");
+        }
+        System.out.println();
 
+        System.out.print("Elementos pares do vetor: ");
+        for (int i = 0; i < 10; i++) {
+            if (numeros[i] % 2 == 0) {
+                System.out.print(numeros[i] + " ");
+            }
+        }
+        System.out.println();
+
+        int soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += numeros[i];
+        }
+        System.out.println("A Soma dos elementos do vetor: " + soma);
+
+        double media = (double) soma / 10;
+        System.out.println("A Média dos elementos do vetor: " + media);
+
+        leia.close();
+    }
 }
+
--- LacoRepeticao/ExercicioDoWhile.java
+++ LacoRepeticao/ExercicioDoWhile.java
@@ -1,10 +1,22 @@
 package Generation.LacoRepeticao;
+import java.util.Scanner;
 
 public class ExercicioDoWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+        int numero, soma = 0;
 
+        do {
+            System.out.print("Digite um número inteiro ou 0 para sair: ");
+            numero = leia.nextInt();
+
+            if (numero > 0) {
+                soma += numero;
+            }
+        } while (numero != 0);
+
+        System.out.println("A soma dos números positivos digitados é: " + soma);
 	}
 
 }
