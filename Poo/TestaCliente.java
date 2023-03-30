8b3d17e0af0b8ed6529766ee9678cb8fcac885ba
Juliana-placido <juliana.lplacido@icloud.com>
2023-03-29 23:17:26 -0300

    Cliente

---
+++ Poo/TestaCliente.java
@@ -0,0 +1,12 @@
+package Generation.Poo;
+
+public class TestaCliente {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente(1, "Juliana Placido", "Rua Lençóis, 123", "(11) 1234-5678", "juliana@email.com");
+        Cliente cliente2 = new Cliente(2, "Gabriel Placido", "Rua Lençois, 456", "(11) 1111-2222", "gabriel@email.com");
+
+        cliente1.visualizar();
+        System.out.println();
+        cliente2.visualizar();;
+    }
+}
